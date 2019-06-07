7d35d6025f81c5687555327c21ffbd536ca598e1
Jeffery Stager <jffry09@gmail.com>
2019-06-07 17:51:21 -0400

    abstracted away platform information

--- src/window.h
+++ src/window.h
@@ -6,18 +6,10 @@
 #include <xcb/shm.h>
 
 struct window {
-  struct {
-    xcb_connection_t *cn;
-    xcb_setup_t *setup;
-    xcb_screen_t *screen;
-    xcb_window_t wn;
-    xcb_gcontext_t gc;
-    xcb_shm_seg_t shmseg;
-    uint32_t *shm_data;
-  } xcb;
-  char *title;
+  void *internal;
   uint16_t width;
   uint16_t height;
+  char *title;
 };
 
 /* **************************************** */
@@ -29,6 +21,7 @@ int window_init(struct window *w,
 void window_show(struct window *w);
 void window_update(struct window *w);
 uint32_t *window_buffer(struct window *w);
+int window_close(struct window *w);
 /* **************************************** */
 
 
--- src/window_linux.c
+++ src/window_linux.c
@@ -4,67 +4,93 @@
 #include <xcb/shm.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <stdlib.h>
 #include <string.h>
 
-static int setup_xcb(struct window *w) {
+struct xcb {
+  xcb_connection_t *cn;
+  xcb_setup_t *setup;
+  xcb_screen_t *screen;
+  xcb_window_t wn;
+  xcb_gcontext_t gc;
+  xcb_shm_seg_t shmseg;
+  uint32_t *shm_data;
+};
+
+static int init_struct(struct window *w,
+                       char *title,
+                       uint16_t width,
+                       uint16_t height) {
+  memset(w, 0, sizeof(struct window));
+  w->internal = malloc(sizeof(struct xcb));
+  if (!w->internal) return -1;
+  w->width = width;
+  w->height = height;
+  w->title = title;
+  return 0;
+}
+
+static int setup_xcb(struct xcb *xcb) {
   int i, screen_index;
   xcb_screen_iterator_t screen_iter;
 
-  w->xcb.cn = xcb_connect(NULL, &screen_index);
-  if (!w->xcb.cn) return -1;
-  w->xcb.setup = (xcb_setup_t *) xcb_get_setup(w->xcb.cn);
-  screen_iter = xcb_setup_roots_iterator(w->xcb.setup);
+  xcb->cn = xcb_connect(NULL, &screen_index);
+  if (!xcb->cn) return -1;
+  xcb->setup = (xcb_setup_t *) xcb_get_setup(xcb->cn);
+  screen_iter = xcb_setup_roots_iterator(xcb->setup);
   /* Get selected screen index information */
   for (i = 0; i < screen_index; ++i) {
     xcb_screen_next(&screen_iter);
   }
-  w->xcb.screen = screen_iter.data;
+  xcb->screen = screen_iter.data;
   return 0;
 }
 
-static void setup_window(struct window *w,
-                         char *title,
-                         uint16_t width,
-                         uint16_t height) {
-  w->xcb.wn = xcb_generate_id(w->xcb.cn);
-  xcb_create_window(w->xcb.cn,
-                    w->xcb.screen->root_depth,
-                    w->xcb.wn,
-                    w->xcb.screen->root,
+static void setup_window(struct window *w) {
+  struct xcb *xcb;
+
+  xcb = (struct xcb *) w->internal;
+  xcb->wn = xcb_generate_id(xcb->cn);
+  xcb_create_window(xcb->cn,
+                    xcb->screen->root_depth,
+                    xcb->wn,
+                    xcb->screen->root,
                     0, 0,
-                    width, height,
+                    w->width, w->height,
                     0,
                     XCB_WINDOW_CLASS_INPUT_OUTPUT,
-                    w->xcb.screen->root_visual,
+                    xcb->screen->root_visual,
                     0,
                     NULL);
-  xcb_change_property(w->xcb.cn,
+  xcb_change_property(xcb->cn,
                       XCB_PROP_MODE_REPLACE,
-                      w->xcb.wn,
+                      xcb->wn,
                       XCB_ATOM_WM_NAME,
                       XCB_ATOM_STRING,
                       8,
-                      (uint32_t) strlen(title),
-                      title);
-  xcb_flush(w->xcb.cn);
+                      (uint32_t) strlen(w->title),
+                      w->title);
+  xcb_flush(xcb->cn);
 }
 
-static void setup_graphics_context(struct window *w) {
-  w->xcb.gc = xcb_generate_id(w->xcb.cn);
-  xcb_create_gc(w->xcb.cn, w->xcb.gc, w->xcb.wn, 0, NULL);
+static void setup_graphics_context(struct xcb *xcb) {
+  xcb->gc = xcb_generate_id(xcb->cn);
+  xcb_create_gc(xcb->cn, xcb->gc, xcb->wn, 0, NULL);
 }
 
 static int setup_shm_pixmap(struct window *w) {
   int shmid;
+  struct xcb *xcb;
 
+  xcb = (struct xcb *) w->internal;
   shmid = shmget(IPC_PRIVATE,
                  sizeof(uint32_t) * w->width * w->height,
                  IPC_CREAT | 0777);
   if (shmid < 0) return -1;
-  w->xcb.shm_data = shmat(shmid, 0, 0);
-  w->xcb.shmseg = xcb_generate_id(w->xcb.cn);
-  xcb_shm_attach(w->xcb.cn, w->xcb.shmseg, (uint32_t) shmid, 0);
-  xcb_flush(w->xcb.cn);
+  xcb->shm_data = shmat(shmid, 0, 0);
+  xcb->shmseg = xcb_generate_id(xcb->cn);
+  xcb_shm_attach(xcb->cn, xcb->shmseg, (uint32_t) shmid, 0);
+  xcb_flush(xcb->cn);
   shmctl(shmid, IPC_RMID, 0);
   return 0;
 }
@@ -77,40 +103,51 @@ int window_init(struct window *w,
                 char *title,
                 uint16_t width,
                 uint16_t height) {
+  struct xcb *xcb;
+
   if (!w) return -1;
-  memset(w, 0, sizeof(struct window));
-  w->width = width;
-  w->height = height;
-  if (setup_xcb(w)) return -1;
-  setup_window(w, title, width, height);
-  setup_graphics_context(w);
+  if (init_struct(w, title, width, height)) return -1;
+  xcb = (struct xcb *) w->internal;
+  if (setup_xcb((struct xcb *) w->internal)) return -1;
+  setup_window(w);
+  setup_graphics_context((struct xcb *) w->internal);
   if (setup_shm_pixmap(w)) return -1;
-  xcb_flush(w->xcb.cn);
+  xcb_flush(xcb->cn);
   return 0;
 }
 
 void window_show(struct window *w) {
+  struct xcb *xcb;
+
   if (!w) return;
-  xcb_map_window(w->xcb.cn, w->xcb.wn);
-  xcb_flush(w->xcb.cn);
+  xcb = (struct xcb *) w->internal;
+  xcb_map_window(xcb->cn, xcb->wn);
+  xcb_flush(xcb->cn);
 }
 
 void window_update(struct window *w) {
-  xcb_shm_put_image(w->xcb.cn,
-                    w->xcb.wn,
-                    w->xcb.gc,
+  struct xcb *xcb;
+
+  xcb = (struct xcb *) w->internal;
+  xcb_shm_put_image(xcb->cn,
+                    xcb->wn,
+                    xcb->gc,
                     w->width, w->height,
                     0, 0,
                     w->width, w->height,
                     0, 0,
-                    w->xcb.screen->root_depth,
+                    xcb->screen->root_depth,
                     XCB_IMAGE_FORMAT_Z_PIXMAP,
                     0,
-                    w->xcb.shmseg,
+                    xcb->shmseg,
                     0);
-  xcb_flush(w->xcb.cn);
+  xcb_flush(xcb->cn);
 }
 
 uint32_t *window_buffer(struct window *w) {
-  return w ? w->xcb.shm_data : NULL;
+  return w ? ((struct xcb *) w->internal)->shm_data : NULL;
+}
+
+int window_close(struct window *w) {
+  return 0;
 }
