169687b2206f87905585c94495801212024e8245
Jeffery Stager <jffry09@gmail.com>
2019-06-12 18:31:16 -0400

    linux: no crashing on window resize

--- src/window_linux.c
+++ src/window_linux.c
@@ -58,7 +58,7 @@ static int setup_xcb(struct xcb *xcb) {
   if (!xcb->cn) return -1;
   xcb->setup = (xcb_setup_t *) xcb_get_setup(xcb->cn);
   screen_iter = xcb_setup_roots_iterator(xcb->setup);
-  /* Get selected screen index information */
+  /* get selected screen index information */
   for (i = 0; i < screen_index; ++i) {
     xcb_screen_next(&screen_iter);
   }
@@ -95,7 +95,7 @@ static int setup_window(struct window *w) {
                       (uint32_t) strlen(w->title),
                       w->title);
   xcb_flush(xcb->cn);
-  /* Watch for delete event */
+  /* watch for delete event */
   protocol_cookie = xcb_intern_atom(xcb->cn,
                                     0,
                                     strlen("WM_PROTOCOLS"),
@@ -130,11 +130,12 @@ static void setup_graphics_context(struct xcb *xcb) {
   xcb_create_gc(xcb->cn, xcb->gc, xcb->wn, 0, NULL);
 }
 
-static int setup_shm_pixmap(struct window *w) {
+static int setup_shm(struct window *w) {
   int shmid;
   struct xcb *xcb;
 
   xcb = (struct xcb *) w->internal;
+  if (xcb->shm_data) xcb_shm_detach(xcb->cn, xcb->shmseg);
   shmid = shmget(IPC_PRIVATE,
                  sizeof(uint32_t) * w->width * w->height,
                  IPC_CREAT | 0777);
@@ -163,13 +164,17 @@ int window_init(struct window *w,
   if (setup_xcb((struct xcb *) w->internal)) return -1;
   if (setup_window(w)) return -1;
   setup_graphics_context((struct xcb *) w->internal);
-  if (setup_shm_pixmap(w)) return -1;
+  if (setup_shm(w)) return -1;
   xcb_flush(xcb->cn);
   return 0;
 }
 
 void window_deinit(struct window *w) {
+  struct xcb *xcb;
+
   if (!w) return;
+  xcb = (struct xcb *) w->internal;
+  xcb_shm_detach(xcb->cn, xcb->shmseg);
   free(w->internal);
 }
 
@@ -187,9 +192,24 @@ void window_update(struct window *w) {
   xcb_generic_event_t *event;
 
   xcb = (struct xcb *) w->internal;
-  /* Check if window has been closed */
+  /* check if window has been closed */
   while ((event = xcb_poll_for_event(xcb->cn))) {
     switch (event->response_type & ~0x80) {
+      /* resize */
+      case XCB_CONFIGURE_NOTIFY: {
+        struct xcb *xcb;
+        xcb_configure_notify_event_t *e;
+
+        xcb = (struct xcb *) w->internal;
+        e = (xcb_configure_notify_event_t *) event;
+        if (e->width != w->width || e->height != w->height) {
+          w->width = e->width;
+          w->height = e->height;
+          /* realloc shared memory lol */
+          setup_shm(w);
+        }
+        break;
+      }
       case XCB_CLIENT_MESSAGE: {
         xcb_client_message_event_t *e;
 
