758b2694225f5982e5db46e4efa82c930b024635
Jeffery Stager <jffry09@gmail.com>
2019-06-12 16:50:49 -0400

    GPL license info and window deinit

--- src/window.h
+++ src/window.h
@@ -1,3 +1,21 @@
+/* This file is a part of libwindow
+ *
+ * Copyright 2019, Jeffery Stager
+ *
+ * libwindow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libwindow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libwindow.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef WINDOW_H
 #define WINDOW_H
 
@@ -18,6 +36,7 @@ int window_init(struct window *w,
                 char *title,
                 uint16_t width,
                 uint16_t height);
+void window_deinit(struct window *w);
 void window_show(struct window *w);
 void window_update(struct window *w);
 uint32_t *window_buffer(struct window *w);
